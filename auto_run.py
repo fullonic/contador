781a9b13e6c8e3328ed7e4105c8f743153632d5c
fullonic <somnium@riseup.net>
2020-07-27 17:22:57 +0200

    Add contador data scrapper

---
+++ auto_run.py
@@ -0,0 +1,56 @@
+"""Run script during intervals."""
+
+import sys
+import time
+import traceback
+from multiprocessing.pool import ThreadPool
+
+import contador
+
+
+def run_safe(fn):
+    """Deal with unexpected errors or miss behaves.
+
+    There are still some edge cases here may occur some error and to avoid complete
+    crashes, we gracefully deal we them here
+    """
+
+    def _inner(*args):
+        reads = 1
+        while True:
+            try:
+                fn(*args)
+                print(f"Total reads: {reads}")
+                reads += 1
+                time.sleep(10 * 60)  # TODO: import from cfg file
+            except Exception as e:
+                # TODO: Add traceback to log file
+                traceback.print_exc(file=sys.stdout)
+                print(e.args)
+
+    return _inner
+
+
+@run_safe
+def run():
+    """Run script in a single thread."""
+    return contador.read()
+
+
+@run_safe
+def run_multiple(pool):
+    """Run script inside a thread pool."""
+    return contador.read_multiple(pool)
+
+
+if __name__ == "__main__":
+    try:
+        mode = sys.argv[1]
+        if mode == "multiple":
+            pool = ThreadPool(4)
+            run_multiple(pool)
+        else:
+            print(f"mode value error: can't be <{mode}>")
+    except IndexError:
+        # no value passed, run single thread script
+        run()
---
+++ contador.py
@@ -0,0 +1,279 @@
+"""Endesa e-distribution consume info scrapper."""
+
+import datetime
+import json
+import os
+import time
+from collections import defaultdict
+from typing import Dict, List, Tuple
+
+import bs4  # type: ignore
+from fake_useragent import UserAgent  # type: ignore
+from selenium import webdriver  # type: ignore
+from selenium.common.exceptions import (  # type: ignore
+    ElementClickInterceptedException,
+    NoSuchElementException,
+)
+from selenium.webdriver import FirefoxOptions  # type: ignore
+from selenium.webdriver import Firefox, FirefoxProfile
+
+HEADLESS = False
+TIMEOUT = 15
+
+#########################
+# Helper functions
+#########################
+def _to_float(value: str) -> float:
+    """Convert stringified float number to a python float."""
+    return float(value.replace(",", "."))
+
+
+def _actual_read(read_: str) -> float:
+    """Clean power consume value.
+
+    'Potencia Instantánea Actual1,22 kW' -> 1.22
+    """
+    read_ = read_.strip("Potencia Instantánea Actual").strip(" kW")
+    return _to_float(read_)
+
+
+def _relative_percent(percent: str) -> float:
+    percent = percent.strip("%")
+    return _to_float(percent)
+
+
+def _max_power(power: str) -> float:
+    power = power.strip(" kW")
+    return _to_float(power)
+
+
+def _wait_to_be_clickable(driver, selector):
+    """Deal with object present on DOM but not be clickable because spinning gif."""
+    reties = 0
+    while reties < 5:
+        try:
+            btn = driver.find_element_by_css_selector(selector)
+            btn.click()
+            break
+        except ElementClickInterceptedException:
+            time.sleep(0.5)
+            reties += 1
+    return
+
+
+def _read_succeed(driver):
+    """Ensure that "consulta contador" succeed."""
+    status = False
+    try:
+        driver.find_element_by_css_selector(".percent")
+        status = True  # Succeed
+    except NoSuchElementException:
+        # means that error is consumption value is yet not visible
+        # checks if there is a error
+        try:
+            # Checks if there was a error
+            error = driver.find_element_by_css_selector("[title='ENTENDIDO']")
+            error.click()
+            print("Request failed [Error Popup]")
+        except NoSuchElementException:
+            # Probably is still present the spinner gif we run it again
+            print("Still spinner")
+    return status
+
+
+#########################
+# JSON Files
+#########################
+def get_config():
+    """Load script configuration file."""
+    with open("config.json") as f:
+        return json.load(f)
+
+
+def storage(type_) -> dict:
+    """Get users from json file."""
+    db = {"users": "users.json", "results": "results.json"}
+    with open(db[type_], "r") as f:
+        data = json.load(f)
+    return data
+
+
+def save(data):
+    results: dict = storage("results")
+    updated = defaultdict(list)
+    updated.update(results)
+    for k, v in data.items():
+        updated[k].append(v)
+    with open("results.json", "w") as f:
+        json.dump(updated, f)
+
+
+#########################
+# Data collector
+#########################
+
+
+def browser_setup():
+    """Set a driver object."""
+    cfg = get_config()["browser"]
+    profile = FirefoxProfile()
+    user_agent = UserAgent()
+    profile.native_events_enabled = cfg["native_events_enabled"]
+    profile.set_preference("general.useragent.override", user_agent.random)
+    opts = FirefoxOptions()
+    opts.headless = cfg["headless"]
+    driver = Firefox(options=opts)
+    driver.implicitly_wait(cfg["timeout"])
+    return driver
+
+
+def login(driver, user=None, password=None):
+    """Deal with user login."""
+    print("-- Inserindo los datos")
+    if not all([user, password]):
+        user = input("Usuario: ")
+        password = input("Contraseña: ")
+    user_in = driver.find_element_by_name("username")
+    user_in.send_keys(user)
+    password_in = driver.find_element_by_name("password")
+    password_in.send_keys(password)
+    submit = driver.find_element_by_css_selector(".slds-button_brand")
+    submit.click()
+    print("Logged in")
+
+
+def contador_online(driver):
+    """Navigate into consume area."""
+    btn = driver.find_element_by_css_selector(
+        "div.slds-col:nth-child(8) > div:nth-child(1) > div:nth-child(1)"
+    )
+    btn.click()
+    print("Contador Online")
+    driver.find_element_by_name("ActionReconectar").click()
+
+
+def get_actual_consume(
+    username: str, page: str, driver: webdriver
+) -> Tuple[bool, Dict[str, tuple]]:
+    """Extract values from page source after getting readings values."""
+    dt = datetime.datetime.now()
+    try:
+        soup = bs4.BeautifulSoup(page, features="html.parser")
+        actual_read = _actual_read(soup.select(".description")[0].text)
+        percent = _relative_percent(soup.select(".percent")[0].text)
+        max_power = _max_power(soup.select(".max > span:nth-child(1)")[0].text)
+        return (
+            True,
+            {
+                username: (
+                    dt.strftime("%d-%m-%Y_%H:%M:%S"),
+                    actual_read,
+                    percent,
+                    max_power,
+                )
+            },
+        )
+    except IndexError:
+        # It means that was not possible to get information from page
+        # TODO: reschedule task
+        return (
+            False,
+            {username: (False, "Failed to get data", dt.strftime("%d-%m-%Y_%H:%M:%S"))},
+        )
+
+
+def lectura(driver, retry=True):
+    """Request the actual reading to the service.
+
+    If it fails, it will automatically retry one try.
+    TODO: Add a reschedule if it fails twice
+    """
+    print("Requesting reading values ...")
+    _wait_to_be_clickable(driver, "[title='Consultar Contador']")
+    spinner = driver.find_element_by_class_name("slds-spinner_container")
+    count = 0
+    while spinner.is_displayed():
+        count += 1
+        time.sleep(1)
+        if _read_succeed(driver):
+            print("Request Succeed")
+            status = True
+            break
+        else:
+            print("Reading failed")
+            if retry:
+                print("Retrying ...")
+                lectura(driver, False)
+            status = False
+            break
+        print(count)
+    return status
+
+
+def _get_reading(user: dict):
+    start = time.perf_counter()
+    username = user["username"]
+    password = user["password"]
+    # Browser setup
+    driver = browser_setup()
+    driver.get("https://www.edistribucion.com/es/index.html")
+    _wait_to_be_clickable(driver, "li.toggleonopen:nth-child(4)")
+
+    # log in form
+    login(driver, username, password)
+    contador_online(driver)
+    lectura(driver)
+    succeed, values = get_actual_consume(username, driver.page_source, driver)
+    driver.close()
+    print(f"Elapsed total time {time.perf_counter() - start}")
+    print("#" * 20)
+    return succeed, values
+
+
+def save_results(succeed, data):
+    if succeed:
+        save(data)
+
+    else:
+        # TODO: Add retry again
+        pass
+        # failed.append(data)
+
+
+def read():
+    """Single thread script entrypoint."""
+    users = storage("users")
+    total_running = time.perf_counter()
+    results = []
+    failed = []
+    for user in users["users"]:
+        succeed, values = _get_reading(user)
+        save_results(succeed, values)
+
+    print(f"Succeed: {results}")
+    print(f"Failed: {failed}")
+    print(f"Total running time {time.perf_counter() - total_running}")
+
+
+
+def read_multiple(pool):
+    """Threadpool script entrypoint."""
+    users = storage("users")["users"]
+    total_running = time.perf_counter()
+    records = pool.map(_get_reading, users)
+    results = []
+    failed = []
+    for rec in records:
+        succeed, values = rec
+        save_results(succeed, values)
+    print(f"Succeed: {results}")
+    print(f"Failed: {failed}")
+    # TODO:
+    # retry failed ones
+    # improve logging and use the same on for multiple or single
+    # add global in memory save for results succeed and failed
+    print(f"Total running time {time.perf_counter() - total_running}")
+
+
+if __name__ == "__main__":
+    read()  # for testing
