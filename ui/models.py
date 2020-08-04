164a9cd2dd0e19cd5e1ceae919168d328ddfdbce
fullonic <somnium@riseup.net>
2020-08-04 17:47:41 +0200

    Add functions to calculate consume for diff scheds

--- ui/models.py
+++ ui/models.py
@@ -1,4 +1,11 @@
+"""
+
+"""
+
 from ui.app import db
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy import extract, and_, or_
+from typing import List
 
 
 class User(db.Model):
@@ -29,7 +36,6 @@ class User(db.Model):
         }
 
 
-
 class Read(db.Model):
     __tablename__ = "reads"
     id = db.Column(db.Integer(), primary_key=True)
@@ -39,7 +45,67 @@ class Read(db.Model):
     max_power = db.Column(db.Float())
     date = db.Column(db.DateTime())
 
+    @hybrid_property
+    def date_hour(self):
+        return self.date.hour
 
+    @date_hour.expression
+    def date_hour(cls):
+        return extract("hour", cls.date)
+
+    @classmethod
+    def history_instantaneous_consume(cls, id_, timespan=()):
+        """All gross comsuptions stats."""
+        user_data = Read.query.filter_by(user_id=id_).all()
+        return {
+            "max": calculate_max_consumption_peak(user_data),
+            "min": calculate_min_consumption_peak(user_data),
+            "average": calculate_average_consumption(user_data),
+        }
+
+    @classmethod
+    def get_hora_punta(cls, id_: int) -> List[object]:
+        """Filter by hora punta.
+
+        10 - 14 and 18 - 22
+        """
+        return Read.query.filter(
+            Read.user_id == id_,
+            or_(
+                and_(Read.date_hour >= 10, Read.date_hour <= 14),
+                and_(Read.date_hour >= 18, Read.date_hour <= 22),
+            ),
+        ).all()
+
+    @classmethod
+    def get_hora_llana(cls, id_: int) -> List[object]:
+        """Filter by hora llana.
+
+        8 - 10, 14 - 18, 22 - 24
+        """
+        return Read.query.filter(
+            Read.user_id == id_,
+            or_(
+                and_(Read.date_hour >= 8, Read.date_hour <= 10),
+                and_(Read.date_hour >= 14, Read.date_hour <= 18),
+                and_(Read.date_hour >= 22, Read.date_hour <= 24),
+            ),
+        ).all()
+
+    @classmethod
+    def get_hora_valle(cls, id_: int) -> List[object]:
+        """Filter by hora valle.
+
+        0 - 8
+        """
+        return Read.query.filter(
+            Read.user_id == id_, and_(Read.date_hour >= 0, Read.date_hour <= 8),
+        ).all()
+
+
+#########################
+# Helper functions
+#########################
 def add_user(username, password, name):
     """Add new user to db."""
     try:
@@ -48,3 +114,18 @@ def add_user(username, password, name):
         db.session.commit()
     except Exception as e:
         print(e)
+
+
+def calculate_max_consumption_peak(data: list):
+    """Max consumption peak."""
+    return max(row.instantaneous_consume for row in data)
+
+
+def calculate_min_consumption_peak(data: list):
+    """Min consumption peak."""
+    return min(row.instantaneous_consume for row in data)
+
+
+def calculate_average_consumption(data: list):
+    """Average consumption."""
+    return sum(row.instantaneous_consume for row in data) / len(data)
--- ui/tests.py
+++ ui/tests.py
@@ -1,27 +1,36 @@
 from ui.app import db
 from ui.graphs import generate_graphic_axis, generate_graphic, create_plot
+import datetime
 import pytest
+from ui.models import Read, User
+from sqlalchemy import extract, and_, or_
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
 def test_get_all_user_as_dict():
     from ui.models import User
+
     users_list = User.json()
     # assert isinstance(users_list, list)
     assert isinstance(users_list, dict)
     assert len(users_list["users"]) == 30
 
+
 def test_generate_graphic_axis():
     from ui.models import User
-    test_dni = "A5961069L" # pass by js query
+
+    test_dni = "A5961069L"  # pass by js query
     user = User.get_by_dni(test_dni)
     axis = generate_graphic_axis(test_dni, user.reads.all())
     isinstance(axis, tuple)
     isinstance(axis[1], tuple)
     isinstance(axis[1][0], list)
 
+
 def test_generate_graphic():
     from ui.models import User
-    test_dni = "A5961069L" # pass by js query
+
+    test_dni = "A5961069L"  # pass by js query
     user = User.get_by_dni(test_dni)
     dni, axis = generate_graphic_axis(test_dni, user.reads.all())
     graphic = generate_graphic(dni, axis)
@@ -31,6 +40,80 @@ def test_generate_graphic():
 
 def test_create_plot():
     from ui.models import User
-    test_dni = "A5961069L" # pass by js query
+
+    test_dni = "A5961069L"  # pass by js query
     user = User.get_by_dni(test_dni)
     create_plot(test_dni, tuple(user.reads.all()))
+
+
+def test_filter_query_by_data_range():
+    """
+    https://stackoverflow.com/questions/11616260/how-to-get-all-objects-with-a-date-that-fall-in-a-specific-month-sqlalchemy/31641488
+    """
+    start, end = 0, 0
+
+    days = Read.query.filter(
+        Read.user_id == 1,
+        extract("month", Read.date) == 10,
+        extract("month", Read.date) == 2,
+    ).all()
+
+    assert len(days) != 0
+
+
+def test_query_by_consume_times():
+    """Filter by difference times of power consumption price.
+
+    Tests
+    Read.get_hora_punta()
+    Read.get_hora_llana()
+    Read.get_hora_valle()
+
+    https://stackoverflow.com/questions/51451768/sqlalchemy-querying-with-datetime-columns-to-filter-by-month-day-year/51468737
+    """
+    hora_punta = ((10, 14), (18, 22))
+    hora_llana = ((8, 10), (14, 18), (22, 24))
+    hora_valle = ((0, 8),)
+
+    user = User.query.get(1)
+    # hora punta
+    reads = Read.query.filter(
+        Read.user_id == user.id,
+        or_(
+            and_(Read.date_hour >= 10, Read.date_hour <= 14),
+            and_(Read.date_hour >= 18, Read.date_hour <= 22),
+        ),
+    ).all()
+
+    # confirm that all hours are beetwen 10-14 or 18-22
+    for el in reads:
+        assert (10 <= el.date.hour and el.date.hour <= 14) or (
+            18 <= el.date.hour and el.date.hour <= 22
+        )
+        # assert (10 <= el.date.hour >= 14) and (18 <= el.date.hour >= 22)
+
+    assert len(reads) == 8001
+    # hora llana
+    reads = Read.query.filter(
+        Read.user_id == user.id,
+        or_(
+            and_(Read.date_hour >= 8, Read.date_hour <= 10),
+            and_(Read.date_hour >= 14, Read.date_hour <= 18),
+            and_(Read.date_hour >= 22, Read.date_hour <= 24),
+        ),
+    ).all()
+
+    for el in reads:
+        assert (
+            (8 <= el.date.hour and el.date.hour <= 10)
+            or (14 <= el.date.hour and el.date.hour <= 18)
+            or (22 <= el.date.hour and el.date.hour <= 24)
+        )
+
+    # hora valle
+    reads = Read.query.filter(
+        Read.user_id == user.id, and_(Read.date_hour >= 0, Read.date_hour <= 8),
+    ).all()
+
+    for el in reads:
+        assert 0 <= el.date.hour and el.date.hour <= 8
